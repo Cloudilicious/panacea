08157c5de048dd97721841a2922e7f0c9bb237ea
Sharon <sharon.codes@gmail.com>
2017-01-12 19:37:29 +1100

    Update role

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def role_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
   end
 end
